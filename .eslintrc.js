8d1cfee0f2363abfe77b9aa2347b8658a0336316
Steve Reid <steve@appby.design>
2020-02-08 12:34:10 +0000

    add overrides config option for ts/tsx files in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,14 @@ module.exports =  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        // Place to specify ESLint rules that only target typescript files
+      }
+    }
+  ],
   settings:  {
     react:  {
       version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
